216a51b93972f69589616a946a7b24d26dcd0660
smallcode <45945756@qq.com>
2014-03-18 18:55:32 +0800

    过滤site-packages根目录下的egg, txt, pth文件

--- django_sae/management/commands/compress_site_packages.py
+++ django_sae/management/commands/compress_site_packages.py
@@ -5,18 +5,21 @@ import zipfile
 import time
 from distutils.sysconfig import get_python_lib
 from django.core.management.base import NoArgsCommand
-from django_extensions.management.commands import clean_pyc
+from django_extensions.management.commands import clean_pyc, compile_pyc
 
 
-def zip_folder(folder_path, zip_name, include_empty_folder=True, filter_root_func=None):
+def zip_folder(folder_path, zip_name, include_empty_folder=True, filter_root=None, filter_file=None):
     root_length = len(folder_path) + 1
     empty_folders = []
     zip_file = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
     for root, folders, files in os.walk(folder_path):
-        if filter_root_func(root[root_length:]):
+        short_root = root[root_length:]
+        if filter_root is not None and filter_root(short_root):
             continue
         empty_folders.extend([folder for folder in folders if os.listdir(os.path.join(root, folder)) == []])
         for f in files:
+            if filter_file is not None and filter_file(short_root, f):
+                continue
             file_name = os.path.join(root, f)
             zip_file.write(file_name, file_name[root_length:])
         if include_empty_folder:
@@ -31,7 +34,9 @@ class Command(NoArgsCommand):
     help = "Compress site-packages folder to a zip file."
     usage_str = "Usage: ./manage.py compress_site_packages"
     filter_modules = ("_markerlib", "pip", "setuptools", "sae", "MySQLdb", "lxml", "PIL",
-                      "werkzeug", "prettytable", "yaml", "argparse", "grizzled", "sqlcmd")
+                      "werkzeug", "prettytable", "yaml", "argparse", "grizzled", "sqlcmd", "enum",
+                      "test", "tests", "_mysql", "_mysql_exceptions", "_mysql",
+                      "easy_install", "pkg_resources")
 
     def handle(self, path=None, name=None, **options):
         if path is None:
@@ -43,11 +48,21 @@ class Command(NoArgsCommand):
         # http://sae.sina.com.cn/doc/python/runtime.html#id3
         if options.get("clean_pyc", sys.version_info[0:3] != (2, 7, 3)):
             clean_pyc.Command().execute(path=path)
+        else:
+            compile_pyc.Command().execute(path=path)
 
         def filter_root(root):
             module_name = root.split(os.path.sep)[0]
-            if not module_name or module_name in self.filter_modules or ".egg-info" in module_name:
+            if module_name in self.filter_modules or ".egg" in module_name:
                 return True
+            return False
 
-        zip_folder(path, name, True, filter_root)
+        def filter_file(root, f):
+            if not root:
+                file_name, extend_name = os.path.splitext(f)
+                if file_name in self.filter_modules or extend_name in ['.egg', '.txt', '.pth']:
+                    return True
+            return False
+
+        zip_folder(path, name, True, filter_root, filter_file)
         self.stdout.write("compressed success:%s" % name)
