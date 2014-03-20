ddbe8011c69ebbca4c778659f6cd6e92050b3ce4
smallcode <45945756@qq.com>
2014-03-20 23:36:43 +0800

    设置DEFAULT_DB_ALIAS，避免测试时抛出异常

--- django_sae/management/commands/sae_syncdb.py
+++ django_sae/management/commands/sae_syncdb.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from django.db.utils import DEFAULT_DB_ALIAS
 from south.management.commands import syncdb
 from django_sae.management.commands import patch_for_sae_restful_mysql, sae_migrate
 
@@ -6,7 +7,9 @@ from django_sae.management.commands import patch_for_sae_restful_mysql, sae_migr
 class Command(syncdb.Command):
     def handle(self, *args, **kwargs):
         patch_for_sae_restful_mysql()
-        # kwargs.setdefault('migrate', True) # 不起作用
+        # kwargs.setdefault('migrate', True)  # 不起作用
+        # 设置DEFAULT_DB_ALIAS，避免测试时抛出异常：ConnectionDoesNotExist: The connection None doesn't exist
+        kwargs.setdefault('database', DEFAULT_DB_ALIAS)
         super(Command, self).handle(*args, **kwargs)
         sae_migrate.Command().execute()
 
