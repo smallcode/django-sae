61d1cd511ce781fa0d8df4905f51075f9148312f
smallcode <45945756@qq.com>
2014-03-21 15:44:57 +0800

    提取 is_exists 方法

--- tests/management/test_upgrade_requirements.py
+++ tests/management/test_upgrade_requirements.py
@@ -8,17 +8,18 @@ from .base import CommandTestBase
 
 
 class UpgradeRequirementsTestCase(CommandTestBase):
-    def get_package_path(self, package):
-        return os.path.join(get_python_lib(), package)
+    def is_exists(self, package):
+        return os.path.exists(os.path.join(get_python_lib(), package)) or os.path.exists(
+            os.path.join(get_python_lib(), '%s.py' % package))
 
     def uninstall(self, package):
         cmd_name, args = pip.parseopts(['uninstall', '--yes', package])
         UninstallCommand().main(args)
-        self.assertFalse(os.path.exists(self.get_package_path(package)))
+        self.assertFalse(self.is_exists(package))
 
     def test_command(self):
         package = 'requests'
         command = upgrade_requirements.Command()
         self.execute(command, requirements=['tests/requirements.txt'])
-        self.assertTrue(os.path.exists(self.get_package_path(package)))
+        self.assertTrue(self.is_exists(package))
         self.uninstall(package)
