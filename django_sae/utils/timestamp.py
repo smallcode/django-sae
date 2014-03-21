615f35002d2a75b07b4c111ef317d66615b70c37
smallcode <45945756@qq.com>
2014-03-21 15:44:02 +0800

    重构 to_datetime

--- django_sae/utils/timestamp.py
+++ django_sae/utils/timestamp.py
@@ -1,18 +1,19 @@
 # -*- coding:utf-8 -*-
+import time
+from datetime import datetime, timedelta
 from django.conf import settings
 from django.utils import timezone
-import datetime, time
 
 
 def to_datetime(timestamp):
+    dt = datetime.fromtimestamp(long(timestamp))
     if settings.USE_TZ:
-        return datetime.datetime.fromtimestamp(long(timestamp)).replace(tzinfo=timezone.get_current_timezone())
-    else:
-        return datetime.datetime.fromtimestamp(long(timestamp))
+        return dt.replace(tzinfo=timezone.get_current_timezone())
+    return dt
 
 
 def to_timestamp(dt):
-    return int(time.mktime(dt.timetuple())) if isinstance(dt, datetime.datetime) else dt
+    return int(time.mktime(dt.timetuple())) if isinstance(dt, datetime) else dt
 
 
 def make_datetime(**kwargs):
@@ -21,7 +22,7 @@ def make_datetime(**kwargs):
     kwargs: days, seconds, microseconds, milliseconds, minutes, hours, weeks
     如：make_datetime(days=-1)
     """
-    return datetime.datetime.now() + datetime.timedelta(**kwargs)
+    return datetime.now() + timedelta(**kwargs)
 
 
 def make_timestamp(**kwargs):
