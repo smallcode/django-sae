6d523e019f17df52519c5b1b8d21437f1e8bf209
smallcode <45945756@qq.com>
2014-05-17 16:40:49 +0800

    修改SaeMigrateTestCase

--- tests/management/test_sae_migrate.py
+++ tests/management/test_sae_migrate.py
@@ -3,8 +3,8 @@ from django_sae.management.commands import sae_migrate
 from .base import CommandTestBase
 
 
-class CommandsTestCase(CommandTestBase):
-    def test_sae_migrate(self):
+class SaeMigrateTestCase(CommandTestBase):
+    def test_command(self):
         command = sae_migrate.Command()
         self.execute(command, db_dry_run=True)
 
